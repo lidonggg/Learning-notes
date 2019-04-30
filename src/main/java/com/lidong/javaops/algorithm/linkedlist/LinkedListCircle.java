169ea9a434da32c3defcfeae73daec84cec07bfa
lidonggg <1225893478@qq.com>
2019-04-30 16:09:16 +0800

    链表中环的检测

---
+++ src/main/java/com/lidong/javaops/algorithm/linkedlist/LinkedListCircle.java
@@ -0,0 +1,127 @@
+package com.lidong.javaops.algorithm.linkedlist;
+
+/**
+ * @author Ls J
+ * @date 2019/4/30 3:08 PM
+ * 检测链表中环的存在
+ */
+public class LinkedListCircle {
+
+    /**
+     * 检测链表中环的存在
+     * 通过快慢指针的方式，如果两个指针在遍历的过程中能相遇，说明存在环
+     *
+     * @param head 头节点
+     * @return 如果存在环，则返回true
+     */
+    private static boolean testLinkedListCircle(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取环形链表中环的长度
+     *
+     * @param head 链表头节点
+     * @return 环的长度，不存在环则返回0
+     */
+    private static int getLinkedListCircleLength(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        int cLen = 0;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            // 如果快慢指针相遇了，暂停本while循环，让fast不动，slow继续
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        // fast不等于null，代表上一个while是循环提前退出的，存在环
+        if (fast != null && fast.next != null) {
+            // 相遇的时候特殊处理
+            slow = slow.next;
+            // 第一次相遇开始记录cLen
+            cLen = 1;
+            // slow继续移动，直到下次与fast相遇，此时slow每走一步，cLen就+1
+            while (slow != fast) {
+                slow = slow.next;
+                cLen++;
+            }
+        }
+
+        return cLen;
+    }
+
+    /**
+     * 获取环的入口点
+     * 如果单链表有环，按照判断是否有环的思路，当快指针和慢指针相遇时，slow指针肯定没有遍历完链表，而fast指针已经在环内循环了n圈。假设slow指针走了s步，则fast指针走了2*s步，设环长为r，则：
+     * 2*s=s+n*r;
+     * s=n*r;
+     * 设链表头到环入口距离为l，入口处距离相遇点距离为a，则：
+     * s=l+a+mr;
+     * 得：l=（n-m）r - a
+     * 所以，相遇后，如果在链表头和相遇点各设置一个指针，每次走一步，两指针必定相遇，且在相遇第一个点为环入口
+     *
+     * @param head 链表头节点
+     * @return 入口点，不存在则返回null
+     */
+    public static ListNode getLinkedListCircleEntrance(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // 第一次相遇，退出while循环
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        // 此时代表不存在环，直接退出
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        // 将其中一个指针指向链表头部，然后一起步进
+        slow = head;
+        // 相遇则停止while循环，此时就是在入口处
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return fast;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node4;
+
+        System.out.println(getLinkedListCircleLength(node1));
+    }
+}
