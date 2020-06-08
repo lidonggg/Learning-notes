86161054781a28ba8ecccf3241d2289059f87b6e
lidonggg <1225893478@qq.com>
2020-06-08 23:09:36 +0800

    add:面试题23-链表环的入口

---
+++ algorithm-go/src/offerleading2/twopointers/entrance_of_cycle_in_the_list23.go
@@ -0,0 +1,102 @@
+//
+// 面试题 23：链表中环的入口
+//
+// 问题描述：
+// 如果一个链表中包含环，返回环的入口节点
+//
+// @author Ls J
+// @date 2020-06-08 22:33
+//
+package main
+
+import (
+    "fmt"
+    . "lib/common"
+)
+
+func main() {
+    head := &ListNode{
+        Val: 1,
+    }
+    l1 := &ListNode{
+        Val: 2,
+    }
+    l2 := &ListNode{
+        Val: 3,
+    }
+    l3 := &ListNode{
+        Val: 4,
+    }
+    head.Next = l1
+    l1.Next = l2
+    l2.Next = l3
+    l3.Next = head
+
+    entrance := findEntrance2(head)
+    if nil != entrance {
+        fmt.Printf("Found! the value of the node is: %d\n", entrance.Val)
+    }
+}
+
+//
+// 方法一：双指针
+// 如果单链表有环，按照判断是否有环的思路，当快指针和慢指针相遇时，slow 指针肯定没有遍历完链表，而fast指针已经在环内循环了 n 圈。
+// 假设 slow 指针走了 s 步，则 fast 指针走了 2*s 步，设环长为 r，则：
+// 2 * s = s + n * r;
+// s = n * r;
+// 设链表头到环入口距离为l，入口处距离相遇点距离为a，则：
+// s = l + a + m * r;
+// 得：l = (n - m) * r - a
+// 所以，相遇后，如果在链表头和相遇点各设置一个指针，每次走一步，两指针必定相遇，且在相遇第一个点为环入口
+//
+// @param   head    *ListNode   头节点
+// @return 入口节点，如果不存在环，返回 false
+//
+func findEntrance1(head *ListNode) *ListNode {
+    if nil == head || nil == head.Next {
+        return nil
+    }
+
+    slow, fast := head, head
+    for nil != fast && nil != fast.Next {
+        slow, fast = slow.Next, fast.Next.Next
+        if fast == slow {
+            break
+        }
+    }
+
+    // 环不存在
+    if nil == fast || nil == fast.Next {
+        return nil
+    }
+
+    slow = head
+    for slow != fast {
+        slow = slow.Next
+        fast = fast.Next
+    }
+
+    return fast
+}
+
+//
+// 方法二：缓存已遍历过的节点，这样只需要遍历一次链表即可
+//
+// @param   head    *ListNode   头节点
+// @return 入口节点，如果不存在环，返回 false
+//
+func findEntrance2(head *ListNode) *ListNode {
+    if nil == head || nil == head.Next {
+        return nil
+    }
+
+    nodeSet := map[*ListNode]bool{}
+    for nil != head {
+        if nodeSet[head] {
+            return head
+        }
+        nodeSet[head] = true
+        head = head.Next
+    }
+    return nil
+}
